8344ef22f6742a971009b03b7956a74f965e4158
Chovy <r130026168@mail.uic.edu.cn>
2024-02-25 16:15:06 +0800

    "add time funtion in log webset"

--- src/main.rs
+++ src/main.rs
@@ -17,43 +17,51 @@ fn say_hello() -> &'static str {
     "Hello dear world!"
 }
 //fn log_something(filename: &'static str, string: &'static [u8; 12]) -> io::Result<()> {
-    fn formatted_time_entry() -> String {
-        let local: DateTime<Local> = Local::now();
-        let formatted = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
-        formatted
-    }
-    
-    fn record_entry_in_log(filename: &str, bytes: &[u8]) -> io::Result<()> {
-        let mut file = OpenOptions::new().
-                            append(true).
-                            write(true).
-                            create(true).
-                            open(filename)?;
-        file.write_all(bytes)?;
-        Ok(())
-    }
-    
-    fn log_time(filename: &'static str) -> io::Result<()> {
-        let entry = formatted_time_entry();
-        let bytes = entry.as_bytes();
-    
-        record_entry_in_log(filename, &bytes)?;
-        Ok(())
-    }
-fn main() {
-    let mut server = Nickel::new();
-    // server.utilize(router! {
-    //     get "**" => |_req, _res| {
-    //         say_hello()
-    //     }
-    // });
+fn formatted_time_entry() -> String {
+    let local: DateTime<Local> = Local::now();
+    let formatted = local.format("%a, %b %d %Y %I:%M:%S %p\n").to_string();
+    formatted
+}
 
-    // server.listen("127.0.0.1:6767");
+fn record_entry_in_log(filename: &str, bytes: &[u8]) -> io::Result<()> {
+    let mut file = OpenOptions::new().
+                        append(true).
+                        write(true).
+                        create(true).
+                        open(filename)?;
+    file.write_all(bytes)?;
+    Ok(())
+}
+
+fn log_time(filename: &'static str) -> io::Result<String> {
+    let entry = formatted_time_entry();
+    {
+        let bytes = entry.as_bytes();
+
+        record_entry_in_log(filename, &bytes)?;
+    }
+    Ok(entry)
+}
+
+fn do_log_time() -> String {
     match log_time("log.txt") {
-        Ok(..) => println!("File created!"),
-        Err(e) => println!("Error: {}", e)
+        Ok(entry) => format!("Entry Logged: {}", entry),
+        Err(e) => format!("Error: {}", e)
     }
 }
+
+fn main() {
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            do_log_time()
+        }
+    });
+
+    server.listen("127.0.0.1:6767");
+}
+
 
 // fn main() {
 //     let mut server = Nickel::new();
